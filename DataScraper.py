919d9fefe549620f076f908a051d6a6aedaae450
MLGDevelopment <nburmeister@mlgcapital.com>
2020-07-08 14:55:05 -0500

    v0.1

---
+++ DataScraper.py
@@ -0,0 +1,348 @@
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import pandas as pd
+from census import Census
+from us import states
+from urllib.request import urlopen
+from fredapi import Fred
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import ast
+from urllib.request import urlopen
+import requests
+import io
+import os
+
+# fetch_CS_price_index()
+# census_conn = Census(CENSUS_API)
+# num_homes = census_conn.acs5.get(('NAME', 'B25034_010E'), {'for': 'state:{}'.format(states.MD.fips)})
+
+
+FRED_API = "7b01ffec8a8b5fa92a16c5af3f72ff14"
+WALK_SCORE = "655a7c936568b9030f821b9c549ee261"
+
+# Load all CBSAs from external file
+CBSA = pd.read_excel("CBSA_Master.xls")
+fred = Fred(api_key=FRED_API)
+CBSA_consolidated_mapping = pd.pivot_table(CBSA, index=["CBSA Title"])["CBSA Code"]
+
+
+class CensusBureau:
+
+    API_KEY = "92227c1ecf7ca9a88fd6069ff8c8107bc45f842a"
+
+    def __init__(self):
+        self.path = os.path.dirname(os.path.realpath(__file__))
+        self.stored_data = os.path.join(self.path, "data", "stored_data")
+        self.pop_features = open(os.path.join(self.path, "pop_features.txt")).read().split("\n")
+
+    def fetch(self, path):
+        with urlopen(path) as url:
+            res = url.read()
+            p_res = parse_string_list(res)
+        return p_res
+
+    def fetch_population_data(self):
+        """
+        EXAMPLE:
+        https://api.census.gov/data/2019/pep/population?
+        get=POP&
+        for=metropolitan%20statistical%20area/micropolitan%20statistical%20area:*&
+        key=YOUR_KEY_GOES_HERE
+        :return:
+        """
+
+        _base = "https://api.census.gov/data/2019/pep/population?get={GET}&for={FOR}&in={IN}&key={KEY}"
+        _base = "https://api.census.gov/data/2019/pep/population?get={GET}&for={FOR}&key={KEY}"
+        _get = "POP"
+        _for = "metropolitan%20statistical%20area/micropolitan%20statistical%20area:*"
+        path = _base.format(GET=_get, FOR=_for, KEY=self.API_KEY)
+
+    def fetch_population_csv(self):
+        path = "https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/metro/totals/cbsa-est2019-alldata.csv"
+        data = requests.get(path).content
+        # io.BytesIO(data.decode('utf-8'))
+        df = pd.read_csv(io.StringIO(data.decode('latin-1')), converters={'CBSA': str})
+        df["CBSA"] = df["CBSA"].map(int)
+        return df
+
+
+class Mappings:
+
+    def __init__(self):
+
+        self.path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data", "mappings")
+        # ZCTA TO CBSA NOTE: MANY TO MANY RELATIONSHIP
+        # self.zcta_cbsa_df = pd.read_excel(os.path.join(self.path, "ZCTA_CBSA.xlsx"), converters={'ZCTA5': str})
+
+        # ZIP CODE TO COUNTY MAPPING (WITH FIPS)
+        # self.zcta_county_df = pd.read_excel(os.path.join(self.path, "ZCTA_County.xlsx"), converters={'ZCTA5': str})
+
+        # ALL ZIPS CODES
+        # self.zip_codes = pd.read_excel(os.path.join(self.path, "ZIP Codes.xlsx"))
+
+        # CBSA TO STATES MAPPING
+        self.cbsa_fips_df = pd.read_excel(os.path.join(self.path, "CBSA_FIPS.xlsx"))
+
+        # LOAD ALL STATES
+        self.states_df = pd.read_excel(os.path.join(self.path, "States.xlsx"))
+        self.states_df.set_index("fips", inplace=True)
+
+
+def parse_string_list(data):
+    """
+    CONVERTS A STRING REPRESENTED LIST TO AN ACTUAL LIST
+    :return:
+    """
+    txt = data.decode()
+    x = ast.literal_eval(txt)
+    return x
+
+
+# def some_data_fetch():
+#     path = "https://api.census.gov/data/2014/pep/natstprc?get=STNAME,POP&DATE_=7&for=state:*&key={API_KEY}".format(
+#         API_KEY=CENSUS_API)
+#     with urlopen(path) as url:
+#         res = url.read()
+#         p_res = parse_string_list(res)
+#     return p_res
+#
+#
+# def fetch_cbsa_population():
+#     path = "https://api.census.gov/data/2019/pep/population?get=POP&for=metropolitan%20statistical%20area/micropolitan%20statistical%20area:*&key={API_KEY}".format(API_KEY=CENSUS_API)
+#
+#     with urlopen(path) as url:
+#         res = url.read()
+#         p_res = parse_string_list(res)
+#
+#     mapping = {}
+#     for i in range(1, len(p_res)):
+#         _cbsa = p_res[i]
+#         population = _cbsa[0]
+#         cbsa = _cbsa[1]
+#         mapping[int(cbsa)] = int(population)
+#
+#     CBSA["Population"] = CBSA["CBSA Code"].map(mapping)
+
+
+class Axio():
+
+    def __init__(self):
+        self.chrome_options = Options()
+        #self.chrome_options.add_argument("--headless")
+        self.login_path = "https://axio.realpage.com/Home"
+        self.market_trends_path = "https://axio.realpage.com/Report/MarketTrendSearch"
+        self.logged_in = False
+
+        try:
+            self.driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=self.chrome_options)
+        except:
+            self.driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=self.chrome_options)
+
+        self.base = ""
+
+    def login(self, username, password, login_link):
+        """
+        METHOD TO LOG INTO YARDI
+        :param username:
+        :param password:
+        :param login_link:
+        :return:
+        """
+        try:
+            self.driver.get(login_link)
+            username_field = self.driver.find_element_by_id("username")
+            username_field.clear()
+            username_field.send_keys(username)
+            self.driver.find_element_by_id("password").click()
+            self.driver.find_element_by_id("password").send_keys(password)
+            self.driver.find_element_by_id("btnSignIn").click()
+            self.logged_in = True
+            return 1
+        except:
+            return 0
+
+    def mlg_axio_login(self):
+        mlg_username = "nburmeister@mlgcapital.com"
+        mlg_password = "nikMLGC9ejfq7f"
+        return self.login(mlg_username, mlg_password, self.login_path)
+
+    def pull_national_data(self):
+        time.sleep(3)
+        if self.logged_in:
+            self.driver.get(self.market_trends_path)
+            time.sleep(3.5)
+            frequency = self.driver.find_element_by_xpath("//*[@id='body-container']/div/div[2]/div[1]/table/tbody/tr[1]/td[1]/span")
+            # frequency.send_keys("Quarterly\nselect")
+            frequency.send_keys("Annual\nselect")
+
+            report_level = self.driver.find_element_by_xpath("//*[@id='body-container']/div/div[2]/div[1]/table/tbody/tr[1]/td[2]/span")
+            report_level.send_keys("National\nselect")
+
+            start_quarter = self.driver.find_element_by_xpath("//*[@id='body-container']/div/div[2]/div[1]/table/tbody/tr[1]/td[3]/span[1]")
+            start_quarter.send_keys("1st Quarter\nselect")
+
+            start_year = self.driver.find_element_by_xpath("//*[@id='body-container']/div/div[2]/div[1]/table/tbody/tr[1]/td[3]/span[2]")
+            start_year.send_keys("1995\nselect")
+
+            end_quarter = self.driver.find_element_by_xpath("//*[@id='body-container']/div/div[2]/div[1]/table/tbody/tr[1]/td[4]/span[1]")
+            end_quarter.send_keys("2nd Quarter\nselect")
+
+            end_year = self.driver.find_element_by_xpath("//*[@id='body-container']/div/div[2]/div[1]/table/tbody/tr[1]/td[4]/span[2]")
+            end_year.send_keys("2019\nselect")
+
+            self.driver.find_element_by_id("btnMarketSearch").click()
+
+            time.sleep(3)
+
+            # now get table
+
+            d_table = self.driver.find_element_by_xpath("//*[@id='period-wrap-table']")
+            columns = d_table.find_elements(By.TAG_NAME, "td")
+
+            index_col = columns[0].text.split("\n")
+            m_list = [[i] for i in index_col]
+
+            data_col = columns[1].text.split("\n")
+            num_rows = len(index_col)
+
+            s_rows = ["SUMMARY",
+                      "PERFORMANCE TREND",
+                      "Asking Rent",
+                      "Effective Rent",
+                      "Physical Occupancy Rate",
+                      "Rental Revenue Impact",
+                      "Concessions",
+                      "Portfolio Attributes",
+                      "SUPPLY AND DEMAND TREND",
+                      "Job Growth",
+                      "Residential Permitting",
+                      "Job Growth Ratio",
+                      "Single Family Home Affordability",
+                      ]
+
+            actual_data_length = len([i for i in index_col if i not in s_rows])
+
+            quarter = 0
+            r_count = 0
+            d_count = 0
+            while True:
+
+                if r_count < num_rows and index_col[r_count] in s_rows:
+                    m_list[r_count].append("")
+                    r_count += 1
+                else:
+                    if r_count < num_rows and d_count < num_rows:
+                        m_list[r_count].append(data_col[actual_data_length*quarter+d_count])
+                        r_count += 1
+                        d_count += 1
+                    else:
+                        if actual_data_length*quarter+d_count >= len(data_col):
+                            break
+                        r_count = 0
+                        d_count = 0
+                        quarter += 1
+            pd.DataFrame(m_list)
+
+
+class Costar:
+
+    def __init__(self, asset_class, path):
+        self.asset_class = asset_class
+        self.path = path
+
+    def import_data(self):
+        df = pd.read_csv(self.path)
+        if self.asset_class == "multifamily":
+            self.multifamily_scrub(df)
+        elif self.asset_class == "industrial":
+            pass
+        elif self.asset_class == "retail":
+            pass
+        elif self.asset_class == "office":
+            pass
+
+    def multifamily_scrub(self, df):
+        slices = set(df["Slice"])
+
+        master = {}
+        for slice in slices:
+            pass
+
+    def office_scrub(self, df):
+        pass
+
+    def industrial_scrub(self, df):
+        pass
+
+    def retail_scrub(self, df):
+        pass
+
+
+def fetch_CS_price_index():
+    cs_mappings = {38060: "PHXRSA",
+                   31080: "LXXRSA",
+                   41740: "SDXRSA",
+                   41860: "SFXRSA",
+                   19740: "DNXRSA",
+                   47900: "WDXRSA",
+                   33100: "MIXRSA",
+                   45300: "TPXRSA",
+                   12060: "ATXRSA",
+                   16980: "CHXRSA",
+                   14460: "BOXRSA",
+                   19820: "DEXRSA",
+                   33460: "MNXRSA",
+                   16740: "CRXRSA",
+                   29820: "LVXRSA",
+                   35620: "NYXRSA",
+                   17420: "CEXRSA",
+                   38860: "POXRSA",
+                   19100: "DAXRSA",
+                   42660: "SEXRSA"}
+
+    mapping = {}
+    for k, v in cs_mappings.items():
+        type = "json"
+        cbsa = k
+        sid = v
+        # path = "https://api.stlouisfed.org/fred/series?series_id={SERIES_ID}&api_key={API_KEY}&file_type={TYPE}".format(SERIES_ID=sid, API_KEY=FRED_API, TYPE=type)
+
+        series = fred.get_series(sid)
+        # series.index = pd.to_datetime(series.index, unit='D')
+        mapping[cbsa] = series
+
+    # find longest series to build index
+    _max = 0
+    id = 0
+    for k, v in mapping.items():
+        t_max = max(_max, len(mapping[k]))
+        if t_max > _max:
+            id = k
+            _max = t_max
+
+    index = mapping[id].index
+
+    df = pd.DataFrame(index=index)
+
+    for k, v in mapping.items():
+        df[k] = df.index.map(mapping[k])
+
+
+def main():
+    axio = Axio()
+    axio.mlg_axio_login()
+    axio.pull_national_data()
+    # costar_mf = Costar("multifamily", "data/costar/multifamily_all.csv")
+    # costar_mf.import_data()
+    pass
+    # fetch_cbsa_population()
+    # Case-Shiller Home Price Index
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ msa_algos.py
@@ -0,0 +1,168 @@
+from DataScraper import CensusBureau
+from DataScraper import Mappings
+import pandas as pd
+import os
+
+mappings = Mappings()
+
+
+def analyze_population(cb):
+    """
+    Algorithm for analyzing population data in the US
+
+    DATA DOCUMENTATION:
+    https://www.census.gov/programs-surveys/popest/about/glossary.html
+    https://www2.census.gov/programs-surveys/popest/technical-documentation/file-layouts/2010-2019/cbsa-est2019-alldata.pdf
+    :param cb:
+    :return:
+    """
+
+    features = cb.pop_features
+    df = cb.fetch_population_csv()
+    mappings.cbsa_fips_df = mappings.cbsa_fips_df[["cbsacode", "fipsstatecode"]].drop_duplicates()
+    df = df.groupby("CBSA").sum()
+
+    """
+    START RELATIVE ANALYSIS
+    """
+    
+    df_relative_analysis = pd.DataFrame(index=df.index)
+    #df_relative_analysis["FIPS"] = df["FIPS"]
+    #df_relative_analysis["STATE"] = df["STATE"]
+
+    # COMPUTE ANNUAL PERCENT CHANGE
+    df_relative_analysis["POPESTIMATE2011_%CHG"] = df["POPESTIMATE2011"] / df["POPESTIMATE2010"] - 1
+    df_relative_analysis["POPESTIMATE2012_%CHG"] = df["POPESTIMATE2012"] / df["POPESTIMATE2011"] - 1
+    df_relative_analysis["POPESTIMATE2013_%CHG"] = df["POPESTIMATE2013"] / df["POPESTIMATE2012"] - 1
+    df_relative_analysis["POPESTIMATE2014_%CHG"] = df["POPESTIMATE2014"] / df["POPESTIMATE2013"] - 1
+    df_relative_analysis["POPESTIMATE2015_%CHG"] = df["POPESTIMATE2015"] / df["POPESTIMATE2014"] - 1
+    df_relative_analysis["POPESTIMATE2016_%CHG"] = df["POPESTIMATE2016"] / df["POPESTIMATE2015"] - 1
+    df_relative_analysis["POPESTIMATE2017_%CHG"] = df["POPESTIMATE2017"] / df["POPESTIMATE2016"] - 1
+    df_relative_analysis["POPESTIMATE2018_%CHG"] = df["POPESTIMATE2018"] / df["POPESTIMATE2017"] - 1
+    df_relative_analysis["POPESTIMATE2019_%CHG"] = df["POPESTIMATE2019"] / df["POPESTIMATE2018"] - 1
+
+    # LOOK AT ROC OF ANNUAL GROWTH
+    df_relative_analysis["NPOPCHG2011_%CHG"] = df["NPOPCHG2011"] / df["NPOPCHG2010"] - 1
+    df_relative_analysis["NPOPCHG2012_%CHG"] = df["NPOPCHG2012"] / df["NPOPCHG2011"] - 1
+    df_relative_analysis["NPOPCHG2013_%CHG"] = df["NPOPCHG2013"] / df["NPOPCHG2012"] - 1
+    df_relative_analysis["NPOPCHG2014_%CHG"] = df["NPOPCHG2014"] / df["NPOPCHG2013"] - 1
+    df_relative_analysis["NPOPCHG2015_%CHG"] = df["NPOPCHG2015"] / df["NPOPCHG2014"] - 1
+    df_relative_analysis["NPOPCHG2016_%CHG"] = df["NPOPCHG2016"] / df["NPOPCHG2015"] - 1
+    df_relative_analysis["NPOPCHG2017_%CHG"] = df["NPOPCHG2017"] / df["NPOPCHG2016"] - 1
+    df_relative_analysis["NPOPCHG2018_%CHG"] = df["NPOPCHG2018"] / df["NPOPCHG2017"] - 1
+    df_relative_analysis["NPOPCHG2019_%CHG"] = df["NPOPCHG2019"] / df["NPOPCHG2018"] - 1
+
+    # 3 YEAR AND 5 YEAR POPULATION CHANGE AVERAGES
+    _3_YEAR_CHANGE_SLICE = features[24:27]
+    _5_YEAR_CHANGE_SLICE = features[22:27]
+    _3_YEAR_CHANGE = df[df.columns.intersection(_3_YEAR_CHANGE_SLICE)].mean(axis=1)
+    _5_YEAR_CHANGE = df[df.columns.intersection(_5_YEAR_CHANGE_SLICE)].mean(axis=1)
+
+    # GET HIGHEST 3/5 YEAR ABSOLUTES
+    _3_YEAR_ABS_CHANGE_SLICE = features[24:27]
+    _5_YEAR_ABS_CHANGE_SLICE = features[22:27]
+    _3_YEAR_ABS_CHANGE = df[df.columns.intersection(_3_YEAR_ABS_CHANGE_SLICE)].mean(axis=1)
+    _5_YEAR_ABS_CHANGE = df[df.columns.intersection(_5_YEAR_ABS_CHANGE_SLICE)].mean(axis=1)
+
+    # DOMESTIC MIGRATION
+    _3_YEAR_DOMESTIC_MIGRATION = features[74:77]
+    _5_YEAR_DOMESTIC_MIGRATION = features[72:77]
+    _ALL_YEAR_DOMESTIC_MIGRATION = features[67:77]
+    df_relative_analysis["3_YEAR_DOMESTIC_MIGRATION"] = df[df.columns.intersection(_3_YEAR_DOMESTIC_MIGRATION)].mean(axis=1) / _3_YEAR_ABS_CHANGE
+    df_relative_analysis["5_YEAR_DOMESTIC_MIGRATION"] = df[df.columns.intersection(_5_YEAR_DOMESTIC_MIGRATION)].mean(axis=1) / _5_YEAR_ABS_CHANGE
+
+    # INTERNATIONAL MIGRATION
+    _3_YEAR_INTERNATIONAL_MIGRATION = features[64:67]
+    _5_YEAR_INTERNATIONAL_MIGRATION = features[62:67]
+    _ALL_YEAR_INTERNATIONAL_MIGRATION = features[57:67]
+    df_relative_analysis["3_YEAR_INTERNATIONAL_MIGRATION"] = df[df.columns.intersection(_3_YEAR_INTERNATIONAL_MIGRATION)].mean(axis=1) / _3_YEAR_ABS_CHANGE
+    df_relative_analysis["5_YEAR_INTERNATIONAL_MIGRATION"] = df[df.columns.intersection(_5_YEAR_INTERNATIONAL_MIGRATION)].mean(axis=1) / _5_YEAR_ABS_CHANGE
+
+    # MOST DEATH - 3/5 YEAR
+    _3_YEAR_DEATHS = features[44:47]
+    _5_YEAR_DEATHS = features[42:47]
+    _ALL_YEAR_DEATHS = features[37:47]
+    df_relative_analysis["3_YEAR_DEATHS"] = df[df.columns.intersection(_3_YEAR_DEATHS)].mean(axis=1) / _3_YEAR_ABS_CHANGE
+    df_relative_analysis["5_YEAR_DEATHS"] = df[df.columns.intersection(_5_YEAR_DEATHS)].mean(axis=1) / _5_YEAR_ABS_CHANGE
+
+    # MOST BIRTHS - 3/5 YEARS
+    _3_YEAR_BIRTHS = features[34:37]
+    _5_YEAR_BIRTHS = features[32:37]
+    _ALL_YEAR_BIRTHS = features[27:37]
+    df_relative_analysis["3_YEAR_BIRTHS"] = df[df.columns.intersection(_3_YEAR_BIRTHS)].mean(axis=1) / _3_YEAR_ABS_CHANGE
+    df_relative_analysis["5_YEAR_BIRTHS"] = df[df.columns.intersection(_5_YEAR_BIRTHS)].mean(axis=1) / _5_YEAR_ABS_CHANGE
+
+
+    """
+    START ABSOLUTE ANALYSIS
+    """
+
+    df_abs_analysis = pd.DataFrame(index=df.index)
+    #df_abs_analysis["FIPS"] = df["FIPS"]
+    #df_abs_analysis["STATE"] = df["STATE"]
+
+    # DEFINE LOOKBACKS
+    _3_YEAR_LOOKBACK = features[24:27]
+    _5_YEAR_LOOKBACK = features[22:27]
+    _ALL_PERIODS_LOOKBACK = features[17:27]
+
+    # AGGREGATE ABSOLUTES: 1, 3, 5 YEARS
+    df_abs_analysis["2019_ABS_CHANGE"] = df["NPOPCHG2019"]
+    df_abs_analysis["3_YEAR_ABS_CHANGE"] = df[df.columns.intersection(_3_YEAR_LOOKBACK)].sum(axis=1)
+    df_abs_analysis["5_YEAR_ABS_CHANGE"] = df[df.columns.intersection(_5_YEAR_LOOKBACK)].sum(axis=1)
+    df_abs_analysis["ALL_YEAR_ABS_CHANGE"] = df[df.columns.intersection(_ALL_PERIODS_LOOKBACK)].sum(axis=1)
+
+    # BIRTHS
+    df_abs_analysis["2019_ABS_BIRTHS"] = df["BIRTHS2019"]
+    df_abs_analysis["3_YEAR_ABS_BIRTHS"] = df[df.columns.intersection(_3_YEAR_BIRTHS)].sum(axis=1)
+    df_abs_analysis["5_YEAR_ABS_BIRTHS"] = df[df.columns.intersection(_5_YEAR_BIRTHS)].sum(axis=1)
+    df_abs_analysis["ALL_YEAR_ABS_BIRTHS"] = df[df.columns.intersection(_ALL_YEAR_BIRTHS)].sum(axis=1)
+
+    # DEATHS
+    df_abs_analysis["2019_ABS_DEATHS"] = df["DEATHS2019"]
+    df_abs_analysis["3_YEAR_ABS_DEATHS"] = df[df.columns.intersection(_3_YEAR_DEATHS)].sum(axis=1)
+    df_abs_analysis["5_YEAR_ABS_DEATHS"] = df[df.columns.intersection(_5_YEAR_DEATHS)].sum(axis=1)
+    df_abs_analysis["ALL_YEAR_ABS_DEATHS"] = df[df.columns.intersection(_ALL_YEAR_DEATHS)].sum(axis=1)
+
+    # DOMESTIC
+    df_abs_analysis["2019_ABS_DOMESTIC"] = df["DOMESTICMIG2019"]
+    df_abs_analysis["3_YEAR_ABS_DOMESTIC"] = df[df.columns.intersection(_3_YEAR_DOMESTIC_MIGRATION)].sum(axis=1)
+    df_abs_analysis["5_YEAR_ABS_DOMESTIC"] = df[df.columns.intersection(_5_YEAR_DOMESTIC_MIGRATION)].sum(axis=1)
+    df_abs_analysis["ALL_YEAR_ABS_DOMESTIC"] = df[df.columns.intersection(_ALL_YEAR_DOMESTIC_MIGRATION)].sum(axis=1)
+
+    # INTERNATIONAL
+    df_abs_analysis["2019_ABS_INTERNATIONAL"] = df["INTERNATIONALMIG2019"]
+    df_abs_analysis["3_YEAR_ABS_INTERNATIONAL"] = df[df.columns.intersection(_3_YEAR_INTERNATIONAL_MIGRATION)].sum(axis=1)
+    df_abs_analysis["5_YEAR_ABS_INTERNATIONAL"] = df[df.columns.intersection(_5_YEAR_INTERNATIONAL_MIGRATION)].sum(axis=1)
+    df_abs_analysis["ALL_YEAR_ABS_INTERNATIONAL"] = df[df.columns.intersection(_ALL_YEAR_INTERNATIONAL_MIGRATION)].sum(axis=1)
+
+    df_abs_analysis["FIPS"] = df_abs_analysis.index.map(dict(mappings.cbsa_fips_df.values))
+    df_abs_analysis["STATE"] = df_abs_analysis["FIPS"].map(mappings.states_df["state"])
+    df_abs_states = df_abs_analysis.groupby("STATE").sum()
+
+    # SAVE TO EXCEL
+
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data", "reports", "population.xlsx")
+    writer = pd.ExcelWriter(path, engine='xlsxwriter')
+    workbook = writer.book
+    # worksheet = workbook.add_worksheet('relative_CBSA_analysis')
+    # worksheet2 = workbook.add_worksheet('absolute_CBSA_analysis')
+    # worksheet3 = workbook.add_worksheet('state_aggregation')
+
+    # writer["relative_CBSA_analysis"] = worksheet
+    # writer["absolute_CBSA_analysis"] = worksheet2
+    # writer["state_aggregation"] = worksheet3
+
+    df_relative_analysis.to_excel(writer, sheet_name="relative_CBSA_analysis")
+    df_abs_analysis.to_excel(writer, sheet_name="absolute_CBSA_analysis")
+    df_abs_states.to_excel(writer, sheet_name="state_aggregation")
+    writer.save()
+
+
+def main():
+    cb = CensusBureau()
+    analyze_population(cb)
+
+
+if __name__ == "__main__":
+    main()
